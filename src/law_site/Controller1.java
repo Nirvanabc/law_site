5c819760f72cf5364aab2d81e273c87f37c3d0c5
Nirvana <muinova@yandex.ru>
2019-05-23 19:19:42 +0300

    each client page in initial varaint

--- src/law_site/Controller1.java
+++ src/law_site/Controller1.java
@@ -1,5 +1,6 @@
 package law_site;
 
+import java.text.ParseException;
 import java.util.List;
 
 import org.hibernate.HibernateException;
@@ -8,12 +9,58 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/")
 public class Controller1 {
 	
 	private static LawSiteDAO dao = new LawSiteDAO();
+	
+	public static class ClientInfo{
+		private String name;
+		public String getname() {
+			return name;
+		}
+		public void setname(String s) {
+			this.name = s;
+		}
+	}
+	
+	public static class ClientId{
+		private int clientid;
+		public int getclientid() {
+			return clientid;
+		}
+		public void setclientid(int clientid) {
+			this.clientid = clientid;
+		}
+	}
+	
+	@RequestMapping(value = "/client", method = RequestMethod.GET)
+	public String getLesson(@RequestParam("id") int client_id, ModelMap model) {
+		model.addAttribute("clientinfo", new ClientInfo());
+		model.addAttribute("idstudent", new ClientId());
+		try {
+			Clients client = dao.loadClient(client_id);
+			model.addAttribute("client", client);
+		} catch (HibernateException e) {
+			return "error";
+		}
+		return "client";
+	}
+	
+	@RequestMapping(value = "/client", method = RequestMethod.POST)
+	public String lesson_post(@RequestParam("id") Integer client_id, @ModelAttribute("clientinfo") ClientInfo cin, 
+		ModelMap model) {
+		try {
+			Clients client = dao.loadClient(client_id);
+			model.addAttribute("client", client);
+		}	catch(HibernateException e) {
+			return "error";
+		}
+		return "client";
+	}
 	
 	@RequestMapping(value = "/123", method = RequestMethod.GET)
 	public String get123(ModelMap model) {
---
+++ web/WEB-INF/pages/client.jsp
@@ -0,0 +1,24 @@
+﻿<?xml version="1.0" encoding="UTF-8" ?>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Курс</title>
+    </head>
+    <body>
+    	<div>
+    	<a href = "clients"> К списку клиентов</a>
+    	</div>
+
+    	<div>Данные о клиенте</div>
+    	${client.id}
+    	${client.client_name}
+    	
+    
+    </body>
+</html>
